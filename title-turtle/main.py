60ba06125c63c4c88818010b3b6a4cc59fb3273d
julian-velez <60607869+julian-velez@users.noreply.github.com>
2026-02-11 13:04:30 -0500

    title_python_turtle
    title_python_turtle

---
+++ title-turtle/main.py
@@ -0,0 +1,16 @@
+import turtle  
+# Importamos el módulo turtle completo.
+# Ahora debemos usar turtle.nombre_funcion()
+
+turtle.setup(400, 440, 0, 0)  
+# Configura la ventana:
+# 400 = ancho
+# 440 = alto
+# 0 = posición X en la pantalla
+# 0 = posición Y en la pantalla
+
+turtle.title("turtle is here")  
+# Cambia el título de la ventana
+
+turtle.exitonclick()  
+# La ventana se cierra cuando haces clic
